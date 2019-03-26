da38a60f1abfe3226a3f74b6d1398edc7d44d5d9
peiyan1234 <d05548014@ntu.edu.tw>
2019-03-26 17:47:44 +0800

    Add files via upload

--- volumn_calculator.py
+++ volumn_calculator.py
@@ -10,14 +10,19 @@ Please inform him for the authorizatiion before further utilities.
 Email: Alvin.Li@acer.com / d05548014@ntu.edu.tw
 """
 
-def calculator(filename_json):
+def calculator(filename_json_dir):
     """
     This function calculate the volumn of a left ventricle.
     """
-    labels = get_labels(filename_json)
+    labels_list = get_labels(filename_json_dir)
 
 
-def get_labels(filename_json):
+def get_labels(filename_json_dir):
     """
     This function outputs the coordinations of labels on an ultrasound image
     """
+    filename = glob.glob(filename_json_dir + '\\' + '*.json')
+    with open(filename, encoding='utf-8') as json_file:
+        label_dic = json.load(json_file)
+
+    return label_dic['shapes'][0]['points']
