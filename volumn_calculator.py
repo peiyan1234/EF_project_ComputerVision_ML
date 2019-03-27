308507d2dde729b95bd7241494c0e67d4b02d26f
peiyan1234 <d05548014@ntu.edu.tw>
2019-03-27 16:11:32 +0800

    Add files via upload

--- volumn_calculator.py
+++ volumn_calculator.py
@@ -10,12 +10,19 @@ Please inform him for the authorizatiion before further utilities.
 Email: Alvin.Li@acer.com / d05548014@ntu.edu.tw
 """
 
-def calculator(filename_json_dir):
+vertices_with_max_area = [] # a list of three points whose traingle area is the largest
+apex_cordis = [] # the coordinates of an apex cordis
+
+def LV_volumn_calculator(filename_json_dir):
     """
     This function calculate the volumn of a left ventricle.
     """
     labels_list = get_labels(filename_json_dir)
+    find_the_maxarea_triangle(labels_list)
+    find_the_apex_cordis(ventrics_with_max_area)
+    LV_volumn = Modified_Simpson_calculator(labels_list)
 
+    return LV_volumn
 
 def get_labels(filename_json_dir):
     """
@@ -26,3 +33,20 @@ def get_labels(filename_json_dir):
         label_dic = json.load(json_file)
 
     return label_dic['shapes'][0]['points']
+
+def find_the_maxarea_triangle(labels_list):
+    """
+    """
+    Area = 0
+
+
+
+def find_the_apex_cordis(ventrics_with_max_area):
+    """
+    """
+
+
+def Modified_Simpson_calculator(labels_list):
+    """
+    """
+    return LV_volumn
